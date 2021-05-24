3fcbb7798dcf580f02439a71c0ec093f70ebb2ee
Michael Heidingsfeld <michael@heidingsfeld.net>
2021-05-24 12:53:56 +0200

    Clean-up test implementation

--- test/test_parametric_cubic_spline.cpp
+++ test/test_parametric_cubic_spline.cpp
@@ -31,13 +31,6 @@
 
 using namespace parametric_cubic_spline;
 
-#define PIVOT_POINTS    {1.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0,-1.0}
-#define EVAL_POINTS     {0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0}
-#define EXPECTED_VALUES {1.0000, 0.0000, 0.1634,-0.1274,-0.5328,-0.1792, \
-                        -0.9482,-0.0798,-0.9600, 0.2320,-0.6500, 0.6500, \
-                        -0.2320, 0.9600, 0.0798, 0.9482, 0.1792, 0.5328, \
-                         0.1274,-0.1634, 0.0000,-1.0000};
-
 class TestProblem
 {
 public:
@@ -76,13 +69,13 @@ public:
 };
 
 static const TestProblem test_problem1(
-    { 1.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0,-1.0}, 
+    { 1.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0,-1.0 }, 
     4, 
     2,
     BoundaryCondition::Natural,
     BoundaryCondition::Natural,
-    { 0.0, 0.0},
-    { 0.0, 0.0},    
+    { 0.0, 0.0 },
+    { 0.0, 0.0 },    
     { 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0 },
     { 1.0000, 0.0000, 0.1634,-0.1274,-0.5328,-0.1792, \
      -0.9482,-0.0798,-0.9600, 0.2320,-0.6500, 0.6500, \
@@ -90,13 +83,13 @@ static const TestProblem test_problem1(
       0.1274,-0.1634, 0.0000,-1.0000 }
 );
 static const TestProblem test_problem2(
-    { 1.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0,-1.0}, 
+    { 1.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0,-1.0 }, 
     4, 
     2,
     BoundaryCondition::Hermite,
     BoundaryCondition::Hermite,
-    { 0.0,-1.0},
-    {-1.0, 0.0},
+    { 0.0,-1.0 },
+    {-1.0, 0.0 },
     { 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0 },
     { 1.0000, 0.0000, 0.6352,-0.2268,-0.1424,-0.2784, \
      -0.8576,-0.1116,-1.0731, 0.3003,-0.7917, 0.7917, \
@@ -146,91 +139,3 @@ INSTANTIATE_TEST_SUITE_P(
         test_problem2
     )
 );
-
-/*
-TEST(ParametricCubicSplineTests, VectorDynamicPointsDynamicDims)
-{
-    std::vector<double> p = PIVOT_POINTS;
-    std::vector<double> s = EVAL_POINTS;
-    std::vector<double> x_expected = EXPECTED_VALUES;
-    std::vector<double> x(2*11, 0.0);
-
-    Spline<double, Dynamic, Dynamic> spline;
-    spline.set(p.data(), 4, 2);
-    spline.eval(s.data(), 11, x.data());
-
-    for(std::size_t i = 0; i < x.size(); i++)
-    {
-        EXPECT_LT(fabs(x[i] - x_expected[i]), 0.001);
-    }
-}
-
-TEST(ParametricCubicSplineTests, VectorDynamicPointsDynamicDimsHermite)
-{
-    std::vector<double> p = PIVOT_POINTS;
-    std::vector<double> s = EVAL_POINTS;
-    std::vector<double> x_expected = EXPECTED_VALUES;
-    std::vector<double> x(2*11, 0.0);
-    std::vector<double> zero_bc = {0.0, 0.0};
-
-    Spline<double, Dynamic, Dynamic> spline;
-    spline.set(p.data(), 4, 2, BoundaryCondition::Hermite, BoundaryCondition::Hermite, zero_bc.data(), zero_bc.data());
-    spline.eval(s.data(), 11, x.data());
-
-    for(std::size_t i = 0; i < x.size(); i++)
-    {
-        EXPECT_LT(fabs(x[i] - x_expected[i]), 0.001);
-    }
-}
-
-TEST(ParametricCubicSplineTests, VectorDynamicPointsFixedDims)
-{
-    std::vector<double> p = PIVOT_POINTS;
-    std::vector<double> s = EVAL_POINTS;
-    std::vector<double> x_expected = EXPECTED_VALUES;
-    std::vector<double> x(2*11, 0.0);
-
-    Spline<double, Dynamic, 2> spline;
-    spline.set(p.data(), 4);
-    spline.eval(s.data(), 11, x.data());
-
-    for(std::size_t i = 0; i < 2*11; i++)
-    {
-        EXPECT_LT(fabs(x[i] - x_expected[i]), 0.001);
-    }
-}
-
-TEST(ParametricCubicSplineTests, DynamicCArray)
-{
-    double p[2*4] = PIVOT_POINTS;
-    double s[11] = EVAL_POINTS;
-    double x_expected[2*11] = EXPECTED_VALUES;
-    double x[2*11] = {0.0};
-
-    Spline<double, Dynamic, Dynamic> spline;
-    spline.set(p, 4, 2);
-    spline.eval(s, 11, x);
-
-    for(std::size_t i = 0; i < 2*11; i++)
-    {
-        EXPECT_LT(fabs(x[i] - x_expected[i]), 0.001);
-    }
-}
-
-TEST(ParametricCubicSplineTests, StaticCArray)
-{
-    double p[2*4] = PIVOT_POINTS;
-    double s[11] = EVAL_POINTS;
-    double x_expected[2*11] = EXPECTED_VALUES;
-    double x[2*11] = {0.0};
-
-    Spline<double, 4, 2> spline;
-    spline.set(p);
-    spline.eval(s, 11, x);
-
-    for(std::size_t i = 0; i < 2*11; i++)
-    {
-        EXPECT_LT(fabs(x[i] - x_expected[i]), 0.001);
-    }
-}
-*/
